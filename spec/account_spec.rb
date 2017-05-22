243519b443bcf87a3aed9578c76f2f69e4666a93
jglass <glassj67@gmail.com>
2017-05-22 11:30:19 -0700

    Add a spec for account balance

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -3,4 +3,13 @@ require File.expand_path('../account.rb', File.dirname(__FILE__))
 
 describe Account do
 
+  context 'with a cat' do
+    let(:cat) { Cat.new("Fluffy") }
+    subject { Account.new(100, "John Smith", cat) }
+
+    it 'bills correctly' do
+      expect{ subject.bill! }.to change(subject.balance).from(100).to(72.50)
+    end
+  end
+
 end
